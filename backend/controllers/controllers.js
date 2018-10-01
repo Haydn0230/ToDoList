4c217f8a19a8d7711be33ab02b1472d9c2add297
haydn brierley-jones <haydn.brierley-jones@skybettingandgaming.com>
2018-10-01 11:38:13 +0100

    Added code structure to Controllers

---
+++ backend/controllers/controllers.js
@@ -0,0 +1,78 @@
+module.exports = {
+    viewAll: (app, req, res) => {
+        app.get('').collection('').find({}).toArray((err, docs)=>{
+            if (err) {
+                console.error(err)
+            }
+            res.json(docs)
+        })
+    },
+    viewSingle: (app,req,res) => {
+        let listID = parseInt(req.params.listID);
+        app.get('').collection('').find({"listID":listID}).toArray((err,docs)=>{
+            if (err) {
+                console.error(err)
+            }
+            res.json(docs)
+        })
+    },
+    addListItem: (app, req, res) => {
+        let newListItem = req.body;
+        let listID = parseInt(newListItem.listID);
+        newListItem.listID = listID;
+
+        app.get('').collection('').insertOne(newListItem, (err,docs) => {
+            if (err) {
+                console.error(err)
+            }
+            res.json({"msg":"Successful"})
+        })
+    },
+    editList: (app,req,res) => {
+        let ammendListItem = req.body;
+        let listID = parseInt(ammendListItem.listID);
+        
+        app.get('').collection('').updateOne(
+            {"listID":listID},
+            {
+                $set: {
+                    "dateAdded": ammendListItem.dateAdded,
+                    "dateForCompletion":dateForCompletion,
+                    "listCategory":listCategory,
+                    "listItem":listItem,
+                    "listPriority":listPriority
+                }
+            },
+            (err, dbResp) => {
+                if (err) {
+                    console.error(err)
+                }
+                if (dbResp.modifiedCount===1) {
+                    res.json({msg:"successfully amended"})
+                } else {
+                    res.json({msg:"Not Found"})
+                }
+            }
+        )
+    },
+
+    deleteListItem: (app,req,res) => {
+        let removeListItem =req.body;
+        let listID = parseInt(removeListItem.listID);
+        app.get('').collection('').deleteOne(
+            {"listID": listID},
+            (err, dbResp) => {
+                if (err) {
+                    console.error(err)
+                }
+                if (dbResp.deleteCount ===1){
+                    res.json({msg:"Successfully Removed"})
+                } else {
+                    res.json({msg: "Not Found"})
+                }
+            }
+        )
+    }
+
+
+}
