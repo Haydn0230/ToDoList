be130ebf56aed39e0c2db9e522259b48d72e8856
haydn brierley-jones <haydn.brierley-jones@skybettingandgaming.com>
2018-10-09 10:58:35 +0100

    cleaned userSchema code

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -9,39 +9,29 @@ const UserSchema = new schema({
 })
 
 UserSchema.pre('save', function(next){
-    console.log("1");
     let user = this;
-    console.dir(user);
+    
     if (!user.isModified('password')) return next();
-
-    console.log('3 ' + user.password);
 
     bcrypt.hash(user.password,10,(err, hash) => {
 
         if (err) return next(err);
             user.password = hash; 
-            console.log('4 :user pass -- ' + user.password);
             next(); 
     })
 })
 
 UserSchema.pre('findOneAndUpdate', function(next){
-    console.log("17");
     let user = this._update;
-    console.log("1>>>>"+user.password);
-    console.dir(user);
+
     if (!user.password) {
-        console.log(">>>>>>>>>>>>>>>>> 6");
         return next();
     }
-
-    console.log('3 ' + user.password);
 
     bcrypt.hash(user.password,10,function(err, hash) {
 
         if (err) return next(err);
             user.password = hash; 
-            console.log('4 :user pass -- ' + user.password);
             next(); 
     })
 })
