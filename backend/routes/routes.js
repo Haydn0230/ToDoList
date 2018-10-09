b1346d0d011de1cf2ae946157765e2407856c96c
haydn brierley-jones <haydn.brierley-jones@skybettingandgaming.com>
2018-10-09 11:43:00 +0100

    List Schema and List Controller added

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -5,17 +5,20 @@ let myControllersUser =require('../controllers/userControllers.js')
 
 
 router =(app) => {
+    // <<< Users >>>
     app.route('/addUser')
     .post((req,res,next) =>myControllersUser.addUser(req,res,next));
 
     app.route('/verify')
-    .post((req,res,next)=> myControllersUser.postUser(req,res,next))
+    .post((req,res,next)=> myControllersUser.postUser(req,res,next));
     
 
     app.route('/user/:id')
     .get((req,res,next) => myControllersUser.getUser(req,res,next))
     .put((req,res,next)=> myControllersUser.putUser(req,res,next))
     .delete((req,res,next) => myControllersUser.deleteUser(req,res,next));
+
+    // <<< List >>>
 }
 
 
